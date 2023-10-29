c44c745129e3d22af1ba143193cc2bc5b36b0b09
Ragul <liwinrahul@gmail.com>
2023-10-29 19:19:20 +0530

    first commit

---
+++ missingElementInAp.py
@@ -0,0 +1,11 @@
+ def findMissing(self, arr, n):
+        d=(arr[n-1]-arr[0])//n
+        l,u=0.n-1
+        while(l<=u):
+            m=(l+u)//2
+            t=
+            if(arr[i+1]-arr[i]==d):
+                i+=1
+            else:
+                return arr[i]+d
+#tc: O(n),sc: O(1)
